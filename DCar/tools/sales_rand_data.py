92af605c38c35d268dd6a15af91a80d86d263b5f
土豆 <1561799709@qq.com>
2025-03-12 17:20:20 +0800

    DCar tool sales_rand_data optimize return

--- DCar/tools/sales_rand_data.py
+++ DCar/tools/sales_rand_data.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Any
+from typing import Any, cast
 import requests
 
 URL = 'https://www.dongchedi.com/motor/pc/car/rank_data'
@@ -9,15 +9,15 @@ RANK_DATA_TYPE = 11
 async def download(url: str, params: dict[str, Any]):
     try:
       response = requests.get(url, params=params)
-      if response.status_code == 200:
+      if response.status_code == 200: # 状态码不是200，一般估计直接抛出异常了
         data = response.json()
         print('page_info', data['data']['paging'])
-        print('page_list', data['data']['list'])
+        # print('page_list', data['data']['list'])
         if 'paging' in data['data']:
             print("paging键 存在")
         else:
             print("paging键 不存在")
-        return data
+        return cast(dict, data)
 
       else:
           print('异常', response)
@@ -26,7 +26,7 @@ async def download(url: str, params: dict[str, Any]):
               "message": response.text,
               "data": None
           }
-          return res
+          return cast(dict, res)
     except Exception as e:
         print('exception. ：', e)
         res = {
@@ -34,7 +34,7 @@ async def download(url: str, params: dict[str, Any]):
             "message": str(e),
             "data": None
         }
-        return res
+        return cast(dict, res)
 
 
 
@@ -44,6 +44,7 @@ if __name__ == '__main__':
       # 页面参数 count=10&offset=70
       params.update({'count': 10})
       params.update({'offset': 0})
-      await download(URL, params)
+      res = await download(URL, params)
+      print(res)
 
     asyncio.run(main())
